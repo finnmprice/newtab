25177f4535a0527035679a940210005e1cb78641
Finn <finnprice03@gmail.com>
2023-10-02 21:54:16 -0700

    added hex color input and random color button

--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,5 @@
+hex = document.getElementById('hex');
+
 chrome.storage.local.get(["rgb"]).then((result) => {
     rgb = result.rgb;
     if (rgb == undefined)
@@ -7,33 +9,46 @@ chrome.storage.local.get(["rgb"]).then((result) => {
     UpdateValue("g", g);
     UpdateValue("b", b);
     document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+    hex.value = rgbToHex(r,g,b);
 });
 
-rslide = document.getElementById('rslider');
-gslide = document.getElementById('gslider');
-bslide = document.getElementById('bslider');
+rslider = document.getElementById('rslider');
+gslider = document.getElementById('gslider');
+bslider = document.getElementById('bslider');
 
-rslide.onchange = function() {
-    UpdateValue("r", this.value)
-    document.body.style.backgroundColor = "rgb(" + this.value + "," + gslide.value + "," + bslide.value + ")";
-    chrome.storage.local.set({ rgb: [this.value, gslide.value, bslide.value] });
-}
+$('#rslider').on('input', function() {
+    UpdateValue("r", this.value);
+    document.body.style.backgroundColor = "rgb(" + this.value + "," + gslider.value + "," + bslider.value + ")";
+    chrome.storage.local.set({ rgb: [this.value, gslider.value, bslider.value] });
+});
 
-gslide.onchange = function() {
-    UpdateValue("g", this.value)
-    document.body.style.backgroundColor = "rgb(" + rslide.value + "," + this.value + "," + bslide.value + ")";
-    chrome.storage.local.set({ rgb: [rslide.value, this.value, bslide.value] });
-}
+$('#gslider').on('input',function() {
+    UpdateValue("g", this.value);
+    document.body.style.backgroundColor = "rgb(" + rslider.value + "," + this.value + "," + bslider.value + ")";
+    chrome.storage.local.set({ rgb: [rslider.value, this.value, bslider.value] });
+});
 
-bslide.onchange = function() {
-    UpdateValue("b", this.value)
-    document.body.style.backgroundColor = "rgb(" + rslide.value + "," + gslide.value + "," + this.value + ")";
-    chrome.storage.local.set({ rgb: [rslide.value, gslide.value, this.value] });
+$('#bslider').on('input', function() {
+    UpdateValue("b", this.value);
+    document.body.style.backgroundColor = "rgb(" + rslider.value + "," + gslider.value + "," + this.value + ")";
+    chrome.storage.local.set({ rgb: [rslider.value, gslider.value, this.value] });
+});
+
+hex.onchange = function() {
+    document.body.style.backgroundColor = hex.value;
+    hexValue = hexToRgb(hex.value);
+    if (hexValue !== null) {
+        UpdateValue("r", hexValue.r)
+        UpdateValue("g", hexValue.g)
+        UpdateValue("b", hexValue.b)
+    }
 }
 
 function UpdateValue(key, value) {
     document.getElementById(key + 'slider').value = value;
     document.getElementById(key + 'display').innerHTML = value;
+
+    hex.value = rgbToHex(rslider.value, gslider.value, bslider.value);
 }
 
 $(".settingsButton").click(function(){
@@ -43,3 +58,38 @@ $(".settingsButton").click(function(){
 $("#settingsExit").click(function(){
   $(".settings").fadeOut(75);
 });
+
+
+$(".randomButton").click(function(){
+    number = randomInt(50,150);
+    UpdateValue("r", number + (randomInt(0,50) * (Math.round(Math.random()) * 2 - 1)));
+    UpdateValue("g", number + (randomInt(0,50) * (Math.round(Math.random()) * 2 - 1)));
+    UpdateValue("b", number + (randomInt(0,50) * (Math.round(Math.random()) * 2 - 1)));
+    document.body.style.backgroundColor = "rgb(" + rslider.value + "," + gslider.value + "," + bslider.value + ")";
+    chrome.storage.local.set({ rgb: [rslider.value, gslider.value, bslider.value] });
+});
+
+
+function randomInt(min, max) { 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+
+function rgbToHex(r, g, b) {
+  return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
+}
+
+function hexToRgb(hex) {
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+    return r + r + g + g + b + b;
+  });
+
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
